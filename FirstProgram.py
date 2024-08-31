d5185a0ee4512c1620406e047853bffd504c23a6
aaliyahmoore <aaliyahmoore@unomaha.edu>
2024-08-31 19:43:53 +0000

    finished lab 1

--- FirstProgram.py
+++ FirstProgram.py
@@ -1,22 +1,27 @@
 #FirstProgram.py
-#Name:
-#Date:
-#Assignment:
+#Name: Aaliyah
+#Date: 08/31/24
+#Assignment:Lab 1
 
 def main():
   print("First Program")
   #Say hello
-  
+  print("hello")
   #Ask for the user's name
+  name = input("Enter your name: ")
 
   #Use the user's name in the program.
-
+  print("Nice to meet you", name)
   #Ask the user for their age.
+  age = input ("Enter your age: ")
+  age = int(age) # convert from words to numbers
 
   #Tell the user what year they were born in.
+  born = 2024 - age
+  
   #Assume that they have not had their birthday yet this year.
+  print("You were born in", born)
 
-
-#Call the main function if this is the file being run.
-if __name__ == '__main__':
+  #Call the main function if this is the file being run.
+  if __name__ == '__main__':
     main()
--- MadLib.py
+++ MadLib.py
@@ -1,16 +1,22 @@
 #MadLib.py
-#Name:
-#Date:
-#Assignment:
+#Name: Aaliyah
+#Date: 08/31/2024
+#Assignment: Lab 1
 
 def main():
   print("Madlib")
   #Ask user for words
-
+  Noun1 = input("Enter a noun: Yogurt ")
+  Noun2 = input ("Enter a noun: Jackie ")
+  Noun3 = input ("Enter a noun: bike ")
+  Noun4 = input ("Enter a noun: sarah")
+  Noun5 = input ("Enter a noun: candy")
+  Noun6 = input ("Enter a noun: store")
   #Print the story with the user supplied words.
 
-
-
+  print ("I like to eat" + Noun1 + "with my mom" + Noun2 + "at the park")
+  print ("I like to ride my" + Noun3 + "with my friend" + Noun4 + "all the time")
+  print ("I love to eat" + Noun5 + "from the" + Noun6 + "each day")
 #Call the main function if this is the file being run.
 if __name__ == '__main__':
     main()
